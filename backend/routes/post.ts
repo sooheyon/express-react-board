1034d8c5b15a8a5e10dc07f604f54968c641df49
sooheyon <sooheyonjo@gmail.com>
2023-11-18 11:57:25 +0900

    글 조회 router 작성

--- backend/routes/post.ts
+++ backend/routes/post.ts
@@ -6,6 +6,20 @@ const router = express.Router();
 
 const client = new PrismaClient();
 
+const SELECT = {
+  id: true,
+  createdAt: true,
+  title: true,
+  content: true,
+  user: {
+    select: {
+      account: true,
+    },
+  },
+};
+
+const PAGE_COUNT = 10;
+
 //글 생성
 router.post("/", verifyToken, async (req: any, res) => {
   try {
@@ -25,20 +39,43 @@ router.post("/", verifyToken, async (req: any, res) => {
 
     const post = await client.post.create({
       data: { title, content, userID: user.id },
-      select: {
-        id: true,
-        createdAt: true,
-        title: true,
-        content: true,
-        user: {
-          select: {
-            account: true,
-          },
-        },
-      },
+      select: SELECT,
     });
 
     return res.json({ ok: true, post });
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      message: "Server Error",
+    });
+  }
+});
+
+//글 조회
+router.get("/", async (req, res) => {
+  try {
+    const { page } = req.query;
+
+    if (!page || isNaN(+page)) {
+      return res.status(400).json({
+        message: "Wrong Page",
+      });
+    }
+
+    const posts = await client.post.findMany({
+      skip: +page * PAGE_COUNT,
+      take: PAGE_COUNT,
+      orderBy: {
+        id: "desc",
+      },
+      select: SELECT,
+    });
+
+    return res.json({
+      ok: true,
+      posts: posts,
+    });
   } catch (error) {
     console.error(error);
 
