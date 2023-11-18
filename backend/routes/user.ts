5bf644d9af10d4ca1c5a67cb8d877a33f8ff649c
sooheyon <sooheyonjo@gmail.com>
2023-11-18 10:20:36 +0900

    router 기본 포맷

--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -1,4 +1,20 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const router = express.Router();
+
+const client = new PrismaClient();
+
+// 유저 생성
+router.post("/", async (req, res) => {
+  try {
+  } catch (error) {
+    console.error(error);
+    //express는 여기까지만 하면 무한로딩에 빠짐
+    return res.status(500).json({
+      message: "Server Error",
+    });
+  }
+});
+
 export default router;
