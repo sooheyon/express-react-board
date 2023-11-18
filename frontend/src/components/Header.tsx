b63e260374ba8eac3acc644a990a159a2df062db
sooheyon <sooheyonjo@gmail.com>
2023-11-18 14:42:38 +0900

    로그아웃 기능 추가

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,19 +1,27 @@
-import React, { useEffect } from "react";
+import React, { FC, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 interface HeaderProps {
   account: string;
 }
 
-const Header = ({ account }: HeaderProps) => {
+const Header: FC<HeaderProps> = ({ account }) => {
+  const clickSignOut = () => {
+    localStorage.removeItem("token");
+    window.location.reload();
+  };
+
   return (
     <header className="max-w-screen-md mx-auto flex items-center p-4">
       {account ? (
         <div>
-          <span className="font-semibold">h662</span>님 환영합니다!
+          <span className="font-semibold">{account}</span>님 환영합니다!
           <Link className="button-style" to="/create">
             Create
           </Link>
+          <button className="button-style" onClick={clickSignOut}>
+            Sign out
+          </button>
         </div>
       ) : (
         <div>
