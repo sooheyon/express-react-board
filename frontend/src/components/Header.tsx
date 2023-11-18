2eeca9ca2ab9e6b828a972afdd5f22d333abf8d9
sooheyon <sooheyonjo@gmail.com>
2023-11-18 14:19:47 +0900

    로그인 여부에 따라 헤더 분기처리

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -7,21 +7,27 @@ interface HeaderProps {
 
 const Header = ({ account }: HeaderProps) => {
   return (
-    <header className="max-w-screen-md mx-auto flex justify-between items-center p-4">
-      <div>
-        <span className="font-semibold">{account}</span>님 환영합니다!
-        <Link className="button-style" to="/create">
-          Create
-        </Link>
-      </div>
-      <div>
-        <Link className="text-blue-500 hover:text-blue-700" to="/sign-in">
-          Sign In
-        </Link>
-        <Link className="ml-4 text-blue-500 hover:text-blue-700" to="/sign-up">
-          Sign Up
-        </Link>
-      </div>
+    <header className="max-w-screen-md mx-auto flex items-center p-4">
+      {account ? (
+        <div>
+          <span className="font-semibold">h662</span>님 환영합니다!
+          <Link className="button-style" to="/create">
+            Create
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <Link className="text-blue-500 hover:text-blue-700" to="/sign-in">
+            Sign In
+          </Link>
+          <Link
+            className="ml-4 text-blue-500 hover:text-blue-700"
+            to="/sign-up"
+          >
+            Sign Up
+          </Link>
+        </div>
+      )}
     </header>
   );
 };
