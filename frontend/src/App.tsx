6e8137733fd0b5101421185fadcde4b88856ec0d
sooheyon <sooheyonjo@gmail.com>
2023-11-18 12:35:10 +0900

    router 기본 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,19 @@
 import React, { FC } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Main } from "./pages";
 
 const App: FC = () => {
-  return <div className="bg-pink-100">Hello, React</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/sign-in" element={<Main />}></Route>
+        <Route path="/sign-up" element={<Main />}></Route>
+        <Route path="/create" element={<Main />}></Route>
+        <Route path="/:postID" element={<Main />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
