12f1a5e994c2d4391c892cda7992cc724475bdb7
sooheyon <sooheyonjo@gmail.com>
2023-11-18 10:50:56 +0900

    auth 라우터 작성 완료

--- backend/routes/auth.ts
+++ backend/routes/auth.ts
@@ -7,4 +7,53 @@ const router = express.Router();
 
 const client = new PrismaClient();
 
+router.post("/", async (req, res) => {
+  try {
+    const { account, password } = req.body;
+    if (
+      !account ||
+      !password ||
+      account.trim().length === 0 ||
+      password.trim().length === 0
+    ) {
+      return res.status(400).json({
+        message: "",
+      });
+    }
+
+    const user = await client.user.findUnique({
+      where: {
+        account,
+      },
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Not Exist User",
+      });
+    }
+
+    const isComparedPassword = bcrypt.compareSync(password, user.password);
+
+    if (!isComparedPassword) {
+      return res.status(400).json({
+        message: "Incorrect password",
+      });
+    }
+
+    const token = jwt.sign({ account }, process.env.JWT_SECRET!);
+
+    return res.json({
+      ok: true,
+      token,
+    });
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      message: "",
+    });
+  }
+});
+
 export default router;
