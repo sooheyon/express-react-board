50bead04673f5fe71ee1282c993b57af7a1f5ed7
sooheyon <sooheyonjo@gmail.com>
2023-11-18 14:15:20 +0900

    create header

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="max-w-screen-md mx-auto flex justify-between items-center p-4">
+      <div>
+        <span className="font-semibold">h662</span>님 환영합니다!
+        <Link className="button-style" to="/create">
+          Create
+        </Link>
+      </div>
+      <div>
+        <Link className="text-blue-500 hover:text-blue-700" to="/sign-in">
+          Sign In
+        </Link>
+        <Link className="ml-4 text-blue-500 hover:text-blue-700" to="/sign-up">
+          Sign Up
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
