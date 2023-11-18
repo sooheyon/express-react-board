172446ee4849157d0cdc8a39c200d9e54921195a
sooheyon <sooheyonjo@gmail.com>
2023-11-18 10:44:04 +0900

    신규 라우터 기본 포맷 작성

---
+++ backend/routes/auth.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const router = express.Router();
+
+const client = new PrismaClient();
+
+export default router;
