8d9f1ac784edc00d283fa1f62ef9c810d1d950d7
sooheyon <sooheyonjo@gmail.com>
2023-12-09 10:13:00 +0900

    글 수정, 삭제, 댓글 연쇄삭제 기능 구현

--- backend/routes/post.ts
+++ backend/routes/post.ts
@@ -137,7 +137,103 @@ router.get("/:postID", async (req, res) => {
 });
 
 //수정
+router.put("/:postID", verifyToken, async (req: any, res) => {
+  try {
+    const { postID } = req.params;
+    const { title, content } = req.body; //get 요청에서는 크로스 브라우징 이슈 있음
+    const { user } = req;
+
+    if (!postID || isNaN(+postID)) {
+      return res.status(400).json({
+        message: "Not exist post id",
+      });
+    }
+
+    if (
+      (!title || title.trim().length === 0) &&
+      !content &&
+      content.trim().length === 0
+    ) {
+      return res.status(400).json({
+        message: "Not exist data",
+      });
+    }
+
+    const existPost = await client.post.findUnique({
+      where: { id: +postID },
+    });
+
+    if (!existPost || existPost.userID !== user.id) {
+      return res.status(400).json({
+        message: "Not exist post",
+      });
+    }
+
+    const updatedPost = await client.post.update({
+      where: {
+        id: +postID,
+      },
+      data: {
+        title: title ?? existPost.title,
+        content: content ?? existPost.content,
+      },
+      select: SELECT,
+    });
+
+    return res.json({ post: updatedPost });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      message: "Server error",
+    });
+  }
+});
 
 //삭제
+//수정
+router.delete("/:postID", verifyToken, async (req: any, res) => {
+  try {
+    const { postID } = req.params;
+    const { user } = req;
+
+    if (!postID || isNaN(+postID)) {
+      return res.status(400).json({
+        message: "Not exist post id",
+      });
+    }
+
+    const existPost = await client.post.findUnique({
+      where: { id: +postID },
+    });
+
+    if (!existPost || existPost.userID !== user.id) {
+      return res.status(400).json({
+        message: "Not exist post",
+      });
+    }
+
+    //스키마에 보존해주겠다라는 설정 추가 또는 아래 함수 추가
+    await client.comment.deleteMany({
+      where: {
+        postID: existPost.id,
+      },
+    });
+
+    const deletedPost = await client.post.delete({
+      where: {
+        id: +postID,
+      },
+    });
+
+    return res.json({ id: deletedPost.id });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      message: "Server error",
+    });
+  }
+});
 
 export default router;
