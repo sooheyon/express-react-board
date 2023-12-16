3054ebbccabf1bcb1ae9995ca123dd6417e8ec8d
sooheyon <sooheyonjo@gmail.com>
2023-12-16 09:34:03 +0900

    댓글 수정 로직

--- backend/routes/comment.ts
+++ backend/routes/comment.ts
@@ -98,4 +98,69 @@ router.get("/", async (req, res) => {
   }
 });
 
+// 댓글 수정
+router.put("/:commentID", verifyToken, async (req: any, res) => {
+  try {
+    const { commentID } = req.params;
+    const { content } = req.body;
+    const { user } = req; //verifyToken에서 주는 값
+
+    if (!commentID || isNaN(+commentID)) {
+      return res.status(400).json({
+        message: "Not exist comment ID",
+      });
+    }
+
+    if (!content || content.trim().length <= 0) {
+      return res.status(400).json({
+        message: "Not exist content",
+      });
+    }
+
+    const existComment = await client.comment.findUnique({
+      where: {
+        id: +commentID,
+      },
+    });
+
+    if (!existComment || existComment.userID !== user.id) {
+      return res.status(400).json({
+        message: "Not exist comment",
+      });
+    }
+
+    const updatedComment = await client.comment.update({
+      where: {
+        id: +commentID,
+      },
+      data: {
+        content,
+      },
+      select: SELECT,
+    });
+
+    return res.json({
+      comment: updatedComment,
+    });
+  } catch (e) {
+    console.error(e);
+
+    return res.status(500).json({
+      message: "Server error",
+    });
+  }
+});
+
+//댓글 삭제
+router.delete("/:commentID", verifyToken, async (req: any, res) => {
+  try {
+  } catch (e) {
+    console.error(e);
+
+    return res.status(500).json({
+      message: "Server error",
+    });
+  }
+});
+
 export default router;
